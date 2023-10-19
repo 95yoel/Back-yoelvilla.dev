9ee0862264d6f646061298039854b598910f9eb5
Yoel Villa <villayoel@gmail.com>
2023-10-20 01:12:22 +0200

    modularize connection to database

---
+++ database_users.py
@@ -0,0 +1,21 @@
+import json
+import psycopg2
+
+
+
+def connect_to_database():
+
+    # acces to database config on json file
+
+    with open("database_config.json", "r") as file:
+        config = json.load(file)
+
+    # connection to postgresql database with psycopg2
+    conn = psycopg2.connect(
+        host=config.get("database_host"),
+        database=config.get("database_name"),
+        user=config.get("database_user"),
+        password=config.get("database_password")
+    )
+    
+    return conn
--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import psycopg2
-import json
+
+from database_users import connect_to_database
 
 
-# acces to database config on json file
-with open("database_config.json", "r") as file:
-    config = json.load(file)
-
-
-# connection to postgresql database with psycopg2
-conn = psycopg2.connect(
-    host=config.get("database_host"),
-    database=config.get("database_name"),
-    user=config.get("database_user"), 
-    password=config.get("database_password")
-)
-
-
+# Connect to database
+conn = connect_to_database()
 
 app = FastAPI()
 
