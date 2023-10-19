8535cda917834690d4e80ea57be946f9bf867d15
Yoel Villa <villayoel@gmail.com>
2023-10-20 00:35:15 +0200

    connect to postgre

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import psycopg2
+import json
+
+
+# acces to database config on json file
+with open("database_config.json", "r") as file:
+    config = json.load(file)
+
+
+# connection to postgresql database with psycopg2
+conn = psycopg2.connect(
+    host=config.get("database_host"),
+    database=config.get("database_name"),
+    user=config.get("database_user"), 
+    password=config.get("database_password")
+)
+
+
 
 app = FastAPI()
 
@@ -16,6 +34,19 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/check")
+async def check_db_connection():
+    try:
+        # Intenta ejecutar una consulta simple
+        cursor = conn.cursor()
+        cursor.execute("SELECT 'Conexión exitosa'")
+        result = cursor.fetchone()[0]
+        cursor.close()
+        return {"message": result}
+    except Exception as e:
+        return {"error": str(e)}
 
 
 if __name__ == "__main__":
